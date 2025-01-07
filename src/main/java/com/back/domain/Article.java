8c6c787fbf49d1abc1c52937dd1a83611e07a977
kmindev <kangkm0324@gmail.com>
2025-01-07 22:35:30 +0900

    #11 - 엔티티 팩토리 메서드 추가, N:1 로딩 전략 수정
    * 외부에서 엔티티 클래스의 인스턴스를 생성하기 위한 팩토리 메서드를 추가함.
    * N:1 관계의 fetch 타입의 기본값이 즉시로딩 이므로 지연로딩으로 수정함.

--- src/main/java/com/back/domain/Article.java
+++ src/main/java/com/back/domain/Article.java
@@ -14,11 +14,23 @@ public class Article extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // id
 
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    @ManyToOne(optional = false)
     private UserAccount userAccount; // 작성자
 
-    @Column(nullable = false) private String title; // 제목
-    @Column(nullable = false, length = 65535) private String content; // 본문
+    @Column(nullable = false)
+    private String title; // 제목
+    @Column(nullable = false, length = 65535)
+    private String content; // 본문
+
+    private Article(UserAccount userAccount, String title, String content) {
+        this.userAccount = userAccount;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Article newArticle(UserAccount userAccount, String title, String content) {
+        return new Article(userAccount, title, content);
+    }
 
 }
