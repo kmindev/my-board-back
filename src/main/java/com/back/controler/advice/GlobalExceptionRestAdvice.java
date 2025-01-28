5a3a78814b0e7e4cb93470c2c0b527cac903aaa9
kmindev <kangkm0324@gmail.com>
2025-01-28 23:08:42 +0900

    #14 - 댓글 등록 기능 구현
    * 댓글 등록 API 구현

--- src/main/java/com/back/controler/advice/GlobalExceptionRestAdvice.java
+++ src/main/java/com/back/controler/advice/GlobalExceptionRestAdvice.java
@@ -2,10 +2,12 @@ package com.back.controler.advice;
 
 import com.back.controler.dto.reponse.ApiResponse;
 import com.back.exception.ApplicationException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,11 +17,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionRestAdvice {
 
     @ExceptionHandler
+    public ResponseEntity<ApiResponse<Void>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.error(e.getMessage(), e);
+        if (e.getCause() instanceof MismatchedInputException mismatchedInputException) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(ApiResponse.errorWithMessage(
+                            HttpStatus.BAD_REQUEST,
+                            mismatchedInputException.getPath().get(0).getFieldName() + " 필드의 값이 잘못되었습니다.")
+                    );
+        }
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.errorWithMessage(HttpStatus.BAD_REQUEST, "확인할 수 없는 형태의 데이터가 들어왔습니다"));
+    }
+
+    @ExceptionHandler
     public ResponseEntity<ApiResponse<Void>> handleValidationExceptions(MethodArgumentNotValidException e) {
         log.error(e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(ApiResponse.errorWithMessage(HttpStatus.BAD_REQUEST, e.getBindingResult().getFieldError().getDefaultMessage()));
+                .body(ApiResponse.errorWithMessage(HttpStatus.BAD_REQUEST,
+                        e.getBindingResult().getFieldError().getDefaultMessage()));
     }
 
     @ExceptionHandler
